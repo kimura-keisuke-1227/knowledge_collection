f6cde20d60664d71de910c134daf1a41ee0f5a31
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-09 11:41:59 +0900

    プロジェクトテーブルに関する定数クラス

---
+++ app/Classes/Const/DatabaseConst/ProjectTableConst.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\Classes\Const\DatabaseConst;
+
+use Illuminate\Support\Facades\Log;
+use app\Classes\Const\DatabaseConst\CommonDatabaseConst as common;
+
+class ProjectTableConst
+{
+    const CONST_TABLE_NAME_OF_PROJECT = 'projects';
+
+    const CONST_FOREIGN_ID_KEY_OF_PROJECT_ID = 'project_id';
+
+    const CONST_CLM_NAME_OF_PROJECT_TABLE_PROJECT = 'project';
+
+}
