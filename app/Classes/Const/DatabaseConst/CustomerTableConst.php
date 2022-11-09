4af4297a927a8953d8d8837991e0daf57038c0b5
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-09 23:29:26 +0900

    顧客情報

---
+++ app/Classes/Const/DatabaseConst/CustomerTableConst.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\Classes\Const\DatabaseConst;
+
+class CustomerTableConst
+{
+    const CONST_TABLE_NAME_OF_CUSTOMER = 'customers';
+
+    const CONST_FOREIGN_ID_KEY_OF_CUSTOMER_ID = 'customer_id';
+    
+    const CONST_CUSTOMER_TABLE_CLM_NAME_OF_CUSTOMER_NAME = 'customer_name';
+    const CONST_CUSTOMER_TABLE_CLM_NAME_OF_PHONE = 'phone';
+    const CONST_CUSTOMER_TABLE_CLM_NAME_OF_POSTAL_CODE = 'postal_code';
+    const CONST_CUSTOMER_TABLE_CLM_NAME_OF_ADDRESS1 = 'address1';
+    const CONST_CUSTOMER_TABLE_CLM_NAME_OF_ADDRESS2 = 'address2';
+    const CONST_CUSTOMER_TABLE_CLM_NAME_OF_ADDRESS3 = 'address3';
+}
