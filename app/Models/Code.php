99c0ab4d72e210e95774571cdb1923f7d947e389
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-10 00:18:19 +0900

    コードの定数

--- app/Models/Code.php
+++ app/Models/Code.php
@@ -4,20 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Classes\Const\DatabaseConst\CodeTableConst as cs;
+use App\Classes\Const\DatabaseConst\CodeTableConst as cd;
+use App\Classes\Const\DatabaseConst\CommonDatabaseConst as cm;
+use App\Classes\Const\DatabaseConst\CategoryTableConst as ct;
 
 class Code extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        cs::CONST_CLM_NAME_OF_CODE_TABLE_CODE,
-        cs::CONST_CLM_NAME_OF_CODE_TABLE_COUNT,
-        cs::CONST_CLM_NAME_OF_CODE_TABLE_EXPLAIN,
-        cs::CONST_CLM_NAME_OF_CODE_TABLE_IMPORTANCE,
-        cs::CONST_CLM_NAME_OF_CODE_TABLE_LANGUAGE_ID,
-        cs::CONST_CLM_NAME_OF_CODE_TABLE_ORDER,
-        cs::CONST_CLM_NAME_OF_CODE_TABLE_SUMMARY
+        cd::CONST_CLM_NAME_OF_CODE_TABLE_CODE,
+        ct::CONST_FOREIGN_ID_KEY_OF_CATEGORY_ID,
+        cm::CONST_COMMON_CLM_NAME_COUNT,
+        cm::CONST_COMMON_CLM_NAME_SUMMARY,
+        cm::CONST_COMMON_CLM_NAME_IMPORTANCE,
+        cm::CONST_COMMON_CLM_NAME_ORDER,
     ];
 
     public function language(){
