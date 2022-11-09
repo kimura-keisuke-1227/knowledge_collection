697aa87a3483d7bfece42a0f1f2d39793c0df74c
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-09 11:23:44 +0900

    ログアウト実装

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,47 +18,51 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        Log::info(__METHOD__.'('.__LINE__.') start by user(' . Util::getUserId() .')');
+        Log::info(__METHOD__ . '(' . __LINE__ . ') start by user(' . Util::getUserId() . ')');
         // バリデーション(フォームリクエストに書き換え可)
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
 
-        Log::debug(__METHOD__.'('.__LINE__.') $request');
+        Log::debug(__METHOD__ . '(' . __LINE__ . ') $request');
         Log::debug($request);
-        
+
         // ログイン情報が正しいか
         // Auth::attemptメソッドでログイン情報が正しいか検証
         if (Auth::attempt($credentials)) {
             // セッションを再生成する処理(セキュリティ対策)
             $request->session()->regenerate();
-            
+
             // ミドルウェアに対応したリダイレクト(後述)
             // 下記はredirect('/admin/blogs')に類似
+            Log::notice(__METHOD__.'('.__LINE__.') user(' .Util::getUserId() . ') login !!' );
             return redirect()->intended('codes/create');
         }
-        
+
         // ログイン情報が正しくない場合のみ実行される処理(return すると以降の処理は実行されないため)
         // 一つ前のページ(ログイン画面)にリダイレクト
         // その際にwithErrorsを使ってエラーメッセージで手動で指定する
         // リダイレクト後のビュー内でold関数によって直前の入力内容を取得出来る項目をonlyInputで指定する
-        Log::info(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .')');
+        Log::info(__METHOD__ . '(' . __LINE__ . ') end by user(' . Util::getUserId() . ')');
         return back()->withErrors([
             'email' => 'メールアドレスまたはパスワードが正しくありません',
-            ])->onlyInput('email');
-        }
-        
-        public function logout(Request $request)
-        {
-            // ログアウト処理
-            Auth::logout();
-            // 現在使っているセッションを無効化(セキュリティ対策のため)
-    $request->session()->invalidate();
-    // セッションを無効化を再生成(セキュリティ対策のため)
-    $request->session()->regenerateToken();
+        ])->onlyInput('email');
+    }
 
-    return redirect() -> route('login');
+    public function logout(Request $request)
+    {
+        Log::info(__METHOD__.'('.__LINE__.') start by user(' . Util::getUserId() .')');
+        
+        // ログアウト処理
+        Auth::logout();
+        // 現在使っているセッションを無効化(セキュリティ対策のため)
+        $request->session()->invalidate();
+        // セッションを無効化を再生成(セキュリティ対策のため)
+        $request->session()->regenerateToken();
+        
+        Log::info(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .')');
+        Log::notice(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .') logout !!');
+        return redirect()->route('login');
+    }
 }
-}
- 
