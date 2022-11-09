b8ef1e225a115ef0ac31eaa122d4d975f6ae7cd8
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-10 00:18:29 +0900

    コード登録テーブルの画面

--- resources/views/codes/create.blade.php
+++ resources/views/codes/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.user')
 
-@section('title', '薬品')
+@section('title', 'コード')
 
 @section('content')
 <div class="container">
@@ -8,34 +8,66 @@
         @csrf
         <table class='table table-striped'>
             <tr>
-                <th>言語</th>
-                <td><select name="language_id" id="language_id">
-                    @foreach ($categories as $category)
-                        <option value="{{$language ->id}}">{{$category->category}}</option>
-                    @endforeach
-                </select></td>
+                <th>カテゴリー</th>
+                <td>
+                    <div class="mb-6">
+                        <div class="flex">
+                            <select id="category_id" class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded" name="category_id">
+                                @foreach ($categories as $category)
+                                <option value="{{$category ->id}}">{{$category->category}}</option>
+                                @endforeach
+                            </select>
+                            <div class="pointer-events-none transform -translate-x-full flex items-center px-2 text-gray-500">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20">
+                                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+
             </tr>
             <tr>
                 <td>概要</td>
-                <td><input type="text" name="summary"></td>
+                <td>
+                    <div class="mb-6">
+                        <input id="summary" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="text" name="summary">
+                    </div>
+                </td>
             </tr>
             <tr>
                 <td>説明</td>
-                <td><input type="text" name="explain"></td>
+                <td>
+                    <div class="mb-6">
+                        <input id="explain" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="text" name="explain">
+                    </div>
+                </td>
             </tr>
             <tr>
                 <td>重要度</td>
-                <td><input type="number" name="importance" value="1"></td>
+                <td>
+                    <div class="mb-6">
+                        <input id="importance" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="number" name="importance" value="0">
+                    </div>
+                </td>
             </tr>
             <tr>
                 <td>回数</td>
-                <td><input type="number" name="count" value="1"></td>
+                <td>
+                    <div class="mb-6">
+                        <input id="count" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="number" name="count" value="0">
+                    </div>
+                </td>
             </tr>
         </table>
         <h4>コード</h4>
-        <textarea name="code" id="" cols="100" rows="10"></textarea>
+        <div class="mb-4">
+            <textarea id="code" name="code" class="w-full h-96 p-4 text-xs leading-none resize-none bg-blueGray-50 rounded outline-none border" type="text" placeholder="ご自由にご記入ください"></textarea>
+        </div>
         <br>
-        <button type="submit">登録</button>
+        <div class="ml-auto">
+            <button type="submit" class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">登録</button>
+        </div>
     </form>
 </div>
 @endsection
