b01025b84f7b0e972d72dd59c209b2bc8a86f29e
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-10 21:02:33 +0900

    プロジェクトのアップデートリクエスト

--- app/Http/Requests/UpdateProjectsRequest.php
+++ app/Http/Requests/UpdateProjectsRequest.php
@@ -4,6 +4,10 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+use App\Classes\Const\DatabaseConst\CommonDatabaseConst as cm;
+use App\Classes\Const\DatabaseConst\ProjectTableConst as pr;
+use App\Classes\Const\ValidationConst as vl;
+
 class UpdateProjectsRequest extends FormRequest
 {
     /**
@@ -13,7 +17,7 @@ class UpdateProjectsRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +28,22 @@ class UpdateProjectsRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            pr::CONST_CLM_NAME_OF_PROJECT_TABLE_PROJECT => [
+                vl::CONST_VALIDATION_REQUIRED
+            ],
+            cm::CONST_COMMON_CLM_NAME_ORDER => [
+                vl::CONST_VALIDATION_REQUIRED,
+                vl::CONST_VALIDATION_NUMERIC
+            ],
+            cm::CONST_COMMON_CLM_NAME_STATUS => [],
+            pr::CONST_CLM_NAME_OF_PROJECT_TABLE_START_DATE => [],
+            pr::CONST_CLM_NAME_OF_PROJECT_TABLE_END_DATE => [],
+            cm::CONST_COMMON_CLM_NAME_SUMMARY => [
+                vl::CONST_VALIDATION_REQUIRED,
+            ],
+            cm::CONST_COMMON_CLM_NAME_DETAIL => [
+                vl::CONST_VALIDATION_REQUIRED,
+            ],
         ];
     }
 }
