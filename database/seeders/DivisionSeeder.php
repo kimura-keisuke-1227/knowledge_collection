60a3ad612316d084be04cebce2c9caa38f64c5a4
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-10 17:32:28 +0900

    constを追加

--- database/seeders/DivisionSeeder.php
+++ database/seeders/DivisionSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Classes\Const\DatabaseConst\CommonDatabaseConst as cm;
+use App\Classes\Const\DatabaseConst\DivisionTableConst as dv;
+
 class DivisionSeeder extends Seeder
 {
     /**
