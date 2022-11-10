b4fed7dc911f3b172205aeb16a36cae87e57ddca
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-10 11:43:19 +0900

    アップデートときのルールをコピー storeから

--- app/Http/Requests/UpdateKnowledgeRequest.php
+++ app/Http/Requests/UpdateKnowledgeRequest.php
@@ -24,7 +24,42 @@ class UpdateKnowledgeRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            return [
+                csct::CONST_FOREIGN_ID_KEY_OF_CATEGORY_ID
+                    =>[
+                        vl::CONST_VALIDATION_REQUIRED
+                    ],
+                cm::CONST_COMMON_CLM_NAME_TITLE
+                    =>[
+                        vl::CONST_VALIDATION_REQUIRED,
+                        'max:255',
+                    ],
+                cm::CONST_COMMON_CLM_NAME_SUMMARY
+                    =>[
+                        vl::CONST_VALIDATION_REQUIRED,
+                        'max:255',
+                    ],
+                kn::CONST_CLM_NAME_OF_KNOWLEDGE_TABLE_KNOWLEDGE
+                    =>[
+                        vl::CONST_VALIDATION_REQUIRED
+                    ],
+                cm::CONST_COMMON_CLM_NAME_ORDER
+                    =>[
+                        vl::CONST_VALIDATION_NUMERIC,
+                        vl::CONST_VALIDATION_REQUIRED
+                    ],
+                cm::CONST_COMMON_CLM_NAME_IMPORTANCE
+                    =>[
+                        vl::CONST_VALIDATION_NUMERIC,
+                        vl::CONST_VALIDATION_REQUIRED
+                    ],
+                cm::CONST_COMMON_CLM_NAME_COUNT
+                    =>[
+                    ],
+                cm::CONST_COMMON_CLM_NAME_FINAL_REFERENCE
+                    =>[
+                    ],
+            ];
         ];
     }
 }
