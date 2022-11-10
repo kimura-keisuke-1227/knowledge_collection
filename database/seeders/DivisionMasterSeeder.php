cc353f9f9e59e662e9c4e501e1f3e18a9b381d03
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-10 12:12:34 +0900

    seederの登録

--- database/seeders/DivisionMasterSeeder.php
+++ database/seeders/DivisionMasterSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Classes\Const\DatabaseConst\CommonDatabaseConst as cm;
+use App\Classes\Const\DatabaseConst\DivisionTableConst as dv;
+
 class DivisionMasterSeeder extends Seeder
 {
     /**
@@ -14,6 +17,8 @@ class DivisionMasterSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param =[
+            cm::CONST_COMMON_CLM_NAME_USER_ID,
+        ];
     }
 }
