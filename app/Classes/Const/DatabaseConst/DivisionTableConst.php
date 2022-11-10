731a090ac021dfc80b21dd410af25121ed869b41
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-10 16:18:32 +0900

    division masterのcodeを定数化

--- app/Classes/Const/DatabaseConst/DivisionTableConst.php
+++ app/Classes/Const/DatabaseConst/DivisionTableConst.php
@@ -10,9 +10,17 @@ class DivisionTableConst
     
     const CONST_CLM_NAME_OF_DIVISION_TABLE_DIVISION = 'division';
 
+
     const CONST_TABLE_NAME_OF_DIVISION_MASTERS = 'division_masters';
 
     const CONST_FOREIGN_ID_KEY_OF_DIVISION_MASTER_ID = 'division_master_id';
     
     const CONST_CLM_NAME_OF_DIVISION_MASTERS_TABLE_DIVISION_MASTER = 'division_master';
+    const CONST_CLM_NAME_OF_DIVISION_MASTERS_TABLE_DIVISION_MASTER_CODE = 'division_master_code';
+
+    const CONST_VALUE_DIVISION_MASTER_KNOWLEDGE_STATUS = 'knowledge_status';
+    const CONST_VALUE_DIVISION_MASTER_PROJECT_STATUS = 'project_status';
+    const CONST_VALUE_DIVISION_MASTER_TASK_STATUS = 'project_status';
+    const CONST_VALUE_DIVISION_MASTER_IMPORTANCE = 'importance';
+    const CONST_VALUE_DIVISION_MASTER_URGENCY = 'urgency';
 }
