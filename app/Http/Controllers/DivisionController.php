22faf185f5ce22a0e95317f57d56bd14aa778ff0
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-11 00:11:30 +0900

    divisionをユーザーとコードから取得できるようにした

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -6,6 +6,13 @@ use App\Http\Requests\StoreDivisionRequest;
 use App\Http\Requests\UpdateDivisionRequest;
 use App\Models\Division;
 
+use App\Classes\Const\DatabaseConst\CommonDatabaseConst as cm;
+use App\Classes\Const\DatabaseConst\DivisionTableConst as dv;
+
+use Illuminate\Support\Facades\Log;
+use App\Classes\Util\Util;
+use Exception;
+
 class DivisionController extends Controller
 {
     /**
@@ -82,5 +89,26 @@ class DivisionController extends Controller
     public function destroy(Division $division)
     {
         //
+    }
+
+    public static function getMyDivisionList($user_id,$division_code){
+        Log::info(__METHOD__.'('.__LINE__.') start by user(' . Util::getUserId() .') $division_code:' . $division_code);
+        $divisions = Division::orderBy(cm::CONST_COMMON_CLM_NAME_ORDER);
+        if(true){
+            $divisions = $divisions->where(cm::CONST_COMMON_CLM_NAME_USER_ID , $user_id);
+        }
+
+        if($division_code !== 0){
+            $divisions = $divisions->where(dv::CONST_CLM_NAME_OF_DIVISION_MASTERS_TABLE_DIVISION_MASTER_CODE);
+        }
+
+        try{
+            $divisions = $divisions->get();
+            Log::notice(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') got divisions');
+        } catch(Exception $e){
+            return 'エラーが発生しました。';
+        }
+        Log::info(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .')');
+        return $divisions;
     }
 }
