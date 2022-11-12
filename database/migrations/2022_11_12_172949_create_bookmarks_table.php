3474a1899f02ad622a1c3c2c8c3f6e41f2b22b41
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-12 18:52:01 +0900

    null可能カラムと、デフォルト値

--- database/migrations/2022_11_12_172949_create_bookmarks_table.php
+++ database/migrations/2022_11_12_172949_create_bookmarks_table.php
@@ -20,14 +20,14 @@ return new class extends Migration
         Schema::create(bm::CONST_TABLE_NAME_OF_BOOKMARK, function (Blueprint $table) {
             $table->id();
             $table->foreignId(cm::CONST_COMMON_CLM_NAME_USER_ID);
-            $table->boolean(bm::CONST_BOOKMARK_TABLE_CLM_NAME_OF_IS_URL);
+            $table->boolean(bm::CONST_BOOKMARK_TABLE_CLM_NAME_OF_IS_URL)->default(0);
             $table->string(bm::CONST_BOOKMARK_TABLE_CLM_NAME_OF_BOOKMARK_URL);
             $table->string(cm::CONST_COMMON_CLM_NAME_TITLE);
-            $table->string(cm::CONST_COMMON_CLM_NAME_SUMMARY);
-            $table->text(bm::CONST_BOOKMARK_TABLE_CLM_NAME_OF_MEMO);
-            $table->foreignId(ct::CONST_FOREIGN_ID_KEY_OF_CATEGORY_ID);
-            $table->integer(cm::CONST_COMMON_CLM_NAME_ORDER);
-            $table->integer(cm::CONST_COMMON_CLM_NAME_IMPORTANCE);
+            $table->string(cm::CONST_COMMON_CLM_NAME_SUMMARY)->nullable();
+            $table->text(bm::CONST_BOOKMARK_TABLE_CLM_NAME_OF_MEMO)->nullable();
+            $table->foreignId(ct::CONST_FOREIGN_ID_KEY_OF_CATEGORY_ID)->default(0);
+            $table->integer(cm::CONST_COMMON_CLM_NAME_ORDER)->default(0);
+            $table->integer(cm::CONST_COMMON_CLM_NAME_IMPORTANCE)->default(0);
             $table->timestamps();
         });
     }
