86dafcc0974e63aba84e62697da1e2282e35b4aa
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-12 18:58:39 +0900

    バリデーションについて、文字数のデフォルトを追加

--- app/Classes/Const/ValidationConst.php
+++ app/Classes/Const/ValidationConst.php
@@ -8,4 +8,6 @@ class ValidationConst
     const CONST_VALIDATION_NUMERIC = 'numeric';
     const CONST_VALIDATION_CONFIRMED = 'confirmed';
     const CONST_VALIDATION_EMAIL = 'email';
+    const CONST_VALIDATION_STRING_LENGTH_MAX_255 = 'max:255';
+    const CONST_VALIDATION_TEXT_LENGTH_MAX_30000 = 'max:30000';
 }
