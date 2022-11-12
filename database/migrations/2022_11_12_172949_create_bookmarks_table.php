3710d1f78d7bbe49d3376715f0c1f80c7ebf2154
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-12 17:48:40 +0900

    bookmarkのユーザーIDを追加

--- database/migrations/2022_11_12_172949_create_bookmarks_table.php
+++ database/migrations/2022_11_12_172949_create_bookmarks_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
     {
         Schema::create(bm::CONST_TABLE_NAME_OF_BOOKMARK, function (Blueprint $table) {
             $table->id();
+            $table->foreignId(cm::CONST_COMMON_CLM_NAME_USER_ID);
             $table->boolean(bm::CONST_BOOKMARK_TABLE_CLM_NAME_OF_IS_URL);
             $table->string(bm::CONST_BOOKMARK_TABLE_CLM_NAME_OF_BOOKMARK_URL);
             $table->string(cm::CONST_COMMON_CLM_NAME_TITLE);
