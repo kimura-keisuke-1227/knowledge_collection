e6f92c41951844e86337978cb1628304d8b9403f
Keisuke Kimura <kim.ksuke@gmail.com>
2022-11-19 10:34:50 +0900

    テンプレートアイテムを登録するためのマイグレーション

--- database/migrations/2022_11_17_212357_create_template_items_table.php
+++ database/migrations/2022_11_17_212357_create_template_items_table.php
@@ -4,6 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Classes\Const\DatabaseConst\CommonDatabaseConst as cm;
+use App\Classes\Const\DatabaseConst\TemplateMasterTableConst as tm;
+use App\Classes\Const\DatabaseConst\TemplateMasterCategoryTableConst as tmc;
+use App\Classes\Const\DatabaseConst\DivisionTableConst as dv;
+
 return new class extends Migration
 {
     /**
@@ -13,7 +18,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('template_items', function (Blueprint $table) {
+        Schema::create(tm::CONST_TABLE_NAME_OF_TEMPLATE_ITEMS, function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(tm::CONST_FOREIGN_ID_KEY_OF_TEMPLATE_MASTER_HEADERS_ID);
+            $table->integer(cm::CONST_COMMON_CLM_NAME_ORDER);
+            $table->string(tm::CONST_CLM_NAME_OF_CATEGORY_TABLE_TEMPLATE_ITEM);
+            $table->text(cm::CONST_COMMON_CLM_NAME_EXPLAIN)
+                ->nullable();
+            $table->foreignId(dv::CONST_VALUE_DIVISION_MASTER_INPUT_TYPE);
+            $table->integer(cm::CONST_COMMON_CLM_NAME_DIGIT);
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +39,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('template_items');
+        Schema::dropIfExists(tm::CONST_TABLE_NAME_OF_TEMPLATE_ITEMS);
     }
 };
